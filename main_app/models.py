03282d161524184d09cc5488a50f09cf38066c71
gorgeousPotato <lanauzbekov@gmail.com>
2023-09-13 23:30:09 -0700

    lab part 1

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Band(models.Model):
+  name=models.CharField(max_length=100)
+  city=models.CharField(max_length=100)
+  country=models.CharField(max_length=100)
+  formed_in=models.IntegerField()
+
+  def __str__(self):
+    return f"{self.name} ({self.id})"
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Band
 
-# Create your views here.
+
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def bands_index(request):
+  bands = Band.objects.all()
+  return render(request, 'bands/index.html', {
+    'bands': bands
+  })
